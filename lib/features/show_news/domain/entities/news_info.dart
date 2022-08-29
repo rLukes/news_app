1812bcd108b3f27da5543e6fa91c1da16424807e
Ruben Lukes <ruben@sticos.no>
2022-08-29 20:32:52 +0200

    added data model

---
+++ lib/features/show_news/domain/entities/news_info.dart
@@ -0,0 +1,10 @@
+class NewsInfo{
+  final String? title;
+  final String? autor;
+  final String? imageUrl;
+  final String? content;
+  final DateTime dateTime;
+
+  NewsInfo({this.title, this.autor, this.imageUrl, this.content,  required this.dateTime});
+
+}
