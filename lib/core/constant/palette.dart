a75cb8b150e55ab06773c828b529e3ca891711ba
Ruben Lukes <ruben@sticos.no>
2022-08-29 14:39:24 +0200

    color palette and search field

---
+++ lib/core/constant/palette.dart
@@ -0,0 +1,9 @@
+import 'package:flutter/material.dart';
+
+class Palette {
+  static const Color background = Color(0xffE8EDF2);
+  static const Color lightGrey = Colors.blueGrey;
+  static const navyBlue = Colors.lightBlueAccent;
+  static const grey = Colors.grey;
+  static const deepBlue = Colors.deepPurpleAccent;
+}
